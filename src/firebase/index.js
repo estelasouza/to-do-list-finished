e63ad2454999de242d120f50362a657427c28cb2
Daniel Carvalho de Oliveira <dco3@cin.ufpe.br>
2021-07-11 18:04:32 -0300

    add e config firebase no projeto

---
+++ src/firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBKxOG-6vrQnYdNuM9u3iNiQieu_u5tIgw",
+  authDomain: "todolist-finished.firebaseapp.com",
+  projectId: "todolist-finished",
+  storageBucket: "todolist-finished.appspot.com",
+  messagingSenderId: "806544678768",
+  appId: "1:806544678768:web:4f51ce4f107005d5bdb9b5",
+  measurementId: "G-WYH4JZQ17Z"
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth()
